44e4e56a5d8e183aaa629dee08186a452850e6a8
Jackson Parthasarathy <Jackson.Parthasarathy@GridUnited.com>
2025-07-27 00:42:09 -0500

    fix: move 'use client' to top of SpotifyStats

--- src/components/SpotifyStats.tsx
+++ src/components/SpotifyStats.tsx
@@ -1,7 +1,6 @@
+"use client";
 import React, { useEffect, useState } from 'react';
 import { fetchTopArtists, fetchTopSongs, fetchTopGenres } from '../lib/spotifyApi';
-
-"use client";
 
 function SpotifyStats() {
     const [topArtists, setTopArtists] = useState([]);
